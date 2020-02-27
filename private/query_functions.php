c4e9958cbfc3adef30c801ca9c81f3e4a1278643
Isaac Bustamante <izaack.97@gmail.com>
2020-02-27 15:50:36 -0700

    Ya inserta un subject en la base de datos

--- private/query_functions.php
+++ private/query_functions.php
@@ -9,6 +9,18 @@ function find_all_subjects(){
 	return $result;
 }
 
+function find_subject_by_id($id){
+	global $db;
+
+	$sql = "SELECT * FROM subjects ";
+	$sql .= "WHERE id='" . $id . "'"; // Query de consulta
+	$result = mysqli_query($db, $sql); // Ejecutamos la consulta
+	confirm_result_set($result); // Verificamos que se hizo correctamente la consulta
+	$subject = mysqli_fetch_assoc($result); // Convertimos el resultset en array associativo
+	mysqli_free_result($result); // Liberamos la memoria del resultset
+	return $subject; // return an assoc. array
+}
+
 function find_all_pages(){
 	global $db;
 
@@ -17,6 +29,28 @@ function find_all_pages(){
 	$result = mysqli_query($db,$sql);
 	confirm_result_set($result);
 	return $result;
+}
+
+function insert_subject($menu_name, $position, $visible){
+	global $db;
+
+	$sql = "INSERT INTO subjects ";
+	$sql .= "(menu_name, position, visible) ";
+	$sql .= "VALUES (";
+	$sql .= "'" . $menu_name . "',";
+	$sql .= "'" . $position . "',";
+	$sql .= "'" . $visible . "'";
+	$sql .= ")";
+	$result = mysqli_query($db, $sql);
+	// For INSERT statements, $result is true/false
+	if ($result) {
+		return true;
+	} else {
+		// INSERT failed
+		echo mysqli_error($db);
+		db_discconect($db);
+		exit();
+	}
 }
 
 ?>
